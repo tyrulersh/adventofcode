6bbac7bc003e89d2dae113d0f72e1a1480e40267
Tyler Rush <me@tylerlogic.com>
2025-12-06 09:39:26 -0500

    2025: Second attempt on day 1 puzzle. Now its right.

--- 2025/day1/src/safe.hs
+++ 2025/day1/src/safe.hs
@@ -1,18 +1,26 @@
 main :: IO ()
-main = interact (show . (enterCombination (CombinationState 50 0) . words))
+main = interact (show . (enterCombination (CombinationState 50 0 0) . words))
 
 -- The state of the application of a combination. The first is the number to
 -- which the dial is pointed. The second is the accumulation of the number of
 -- times a zero is seen.
 data CombinationState = CombinationState {
     position :: Int,
-    zeros :: Int
+    zeros :: Int,
+    allZeros :: Int
 } deriving (Show)
 
 enterCombination :: CombinationState -> [String] -> CombinationState
 enterCombination start = foldl rotate' start
-    where rotate' state move = toStateWithZerosCheck (zeros state) $ rotate (position state) move
-          toStateWithZerosCheck currentZeros nextPosition = CombinationState nextPosition (currentZeros + if nextPosition == 0 then 1 else 0)
+
+rotate' :: CombinationState -> String -> CombinationState
+rotate' state move@(direction:xs) = CombinationState position' zeros' allZeros'
+    where position' = rotate (position state) move
+          zeros' = (zeros state) + if position' == 0 then 1 else 0
+          allZeros' = (allZeros state) + (quot steps 100) + if position state /= 0 && wraps direction then 1 else 0
+          wraps 'L' = position' > (position state) || position' == 0
+          wraps 'R' = position' < (position state)
+          steps = read xs
 
 rotate :: Int -> String -> Int
 rotate currentPosition ('L':cs) = mod (currentPosition - n) 100
