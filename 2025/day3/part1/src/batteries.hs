17b602cceea625ab162747802ba8356927bcf559
Tyler Rush <me@tylerlogic.com>
2025-12-12 20:53:17 -0500

    2025: Solution to day3 part 2. Wholly re-worked with simpler approach.

--- 2025/day3/part1/src/batteries.hs
+++ 2025/day3/part1/src/batteries.hs
@@ -8,17 +8,19 @@ type Bank = Integer
 banks :: [String] -> [Bank]
 banks = map read
 
-joltage :: Int -> Bank -> Integer
-joltage n bank = last $ last $ take n rowsOfDigits
-  where rowsOfDigits = firstRow:(zipWith3 attempting [2..] rowsOfDigits (repeat reverseDigits))
-        reverseDigits = map readDigit $ reverse $ show bank
-        firstRow = (reverseDigits !! 0):(zipWith max firstRow (tail reverseDigits))
-        readDigit c = fromIntegral $ ord c - ord '0'
+toDigit :: Char -> Int
+toDigit c = ord c - ord '0'
 
-attempting :: Int -> [Integer] -> [Integer] -> [Integer]
-attempting i previous reverseDigits = take (i - (length maxes)) negs ++ maxes
-  where maxes = (head newPossibleMaxes):(zipWith max' maxes (tail newPossibleMaxes))
-        negs = repeat $ fromIntegral (-1)
-        newPossibleMaxes = zipWith (\p d -> 10*p + d) (drop (i-2) previous) (drop (i-1) reverseDigits)
-        max' n m = max (reverseInteger n) (reverseInteger m)
-        reverseInteger n = read $ reverse $ show n
+joltage :: Int -> Bank -> Integer
+joltage n bank = read $ concat $ map show $ joltage' n (map toDigit $ show bank)
+
+-- takes a bank as a list of Int instead of Bank type
+joltage' :: Int -> [Int] -> [Int]
+joltage' n digits
+  | n == 0 = []
+  | length digits <= n = digits
+  | otherwise = highest : (joltage' (n-1) digits')
+    where negative_indices = map ((*) (-1)) [0..] -- negating the indices allows for getting the left most max on matchin
+          consideration = zip (take ((length digits) - (n - 1)) digits) negative_indices
+          (highest, negative_index) = maximum consideration
+          digits' = drop (-1*negative_index + 1) digits
