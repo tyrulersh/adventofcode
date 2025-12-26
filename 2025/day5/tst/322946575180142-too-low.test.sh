1b03d191f4257411466f4f87fb4288041ee13bf2
Tyler Rush <me@tylerlogic.com>
2025-12-26 12:44:22 -0500

    2025: Many more day5 part2 test cases. Still no new ones failing.

--- 2025/day5/tst/322946575180142-too-low.test.sh
+++ 2025/day5/tst/322946575180142-too-low.test.sh
@@ -1,11 +1,11 @@
 MODE=$1
 # The part2 output needs to be less than this number.
-MAX_RESULT=338693411431457
+MIN_RESULT=322946575180142
 
 if [[ "$MODE" == "assert" ]]; then
   result=$(cat /dev/stdin)
-  if [[ $result -ge $MAX_RESULT ]]; then
-    echo "Answer must be less than $MAX_RESULT, but instead was $result"
+  if [[ $result -le $MIN_RESULT ]]; then
+    echo "Answer must be greater than $MIN_RESULT, but instead was $result"
     exit 1
   fi
 fi
