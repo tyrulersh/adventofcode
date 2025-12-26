85caaa1468b06ba10b1f85f785839a5f90a58ce4
Tyler Rush <me@tylerlogic.com>
2025-12-26 10:43:07 -0500

    2025: Initial solution. Solves part 1, but only simple example of part2 and not main solution.

---
+++ 2025/day5/src/ingredients.hs
@@ -0,0 +1,54 @@
+import Data.List (nub, sort)
+import Data.List.Split (splitOn)
+import Debug.Trace
+
+main :: IO ()
+main = interact (selector . words)
+  where selector ("part1":ws) = part1 ws
+        selector ("part2":ws) = part2 ws
+        part1 = show . length . spoiles . parse
+        part2 = show . freshies . parse
+
+type Fresh = [(Integer,Integer)]
+type Ingredients = [Integer]
+type DB = (Fresh, Ingredients)
+
+parse :: [String] -> DB
+parse = foldl combine ([],[]) . map singleton
+  where singleton s
+          | xs == [] = ([],[read x])
+          | otherwise = ([(read x,read (head xs))],[])
+          where x:xs = splitOn "-" s
+
+combine :: DB -> DB -> DB
+combine (a,b) (x,y) = (a++x,b++y)
+
+--freshies :: DB -> [Integer]
+--freshies ([],_) = []
+--freshies (x:xs,_) = nub $ fullRange x ++ freshies (xs,[])
+--  where fullRange (low,high) = takeWhile ((>=) high) [low..]
+
+freshies :: DB -> Integer
+freshies = freshies' . fst
+  where freshies' = sum . map rangeSize . contiguate . sort
+
+contiguate :: Fresh -> Fresh
+contiguate l = foldr reducer [last l] l
+  where reducer next accum = mergeIf next (head accum) ++ tail accum
+        mergeIf t u@(x,y) = if x `inn` t || y `inn` t then [merge t u] else [t,u]
+        inn x (a,b) = x >= a && x <= b
+        merge (a,b) (x,y) = (min a x, max b y)
+
+rangeSize :: (Integer,Integer) -> Integer
+rangeSize (x,y) = y - x + 1
+
+spoiled :: Fresh -> Integer -> Bool
+spoiled ranges i = dropWhile (outOfRange i) ranges /= []
+  where outOfRange i (low,high) = i < low || i > high
+
+spoiles :: DB -> [Integer]
+spoiles (_, []) = []
+spoiles (ranges, i:is)
+  | spoiled ranges i = i:spoiles'
+  | otherwise = spoiles'
+    where spoiles' = spoiles (ranges, is)
