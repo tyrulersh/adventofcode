a0933221db81bea26a011e833a7bfd5e750bf917
Tyler Rush <me@tylerlogic.com>
2025-12-27 00:09:40 -0500

    2025: Solution to day6 part two.

--- 2025/day6/src/maths.hs
+++ 2025/day6/src/maths.hs
@@ -1,18 +1,25 @@
-import Data.List (nub, sort)
+import Data.List (transpose)
 import Data.List.Split (splitOn)
 
 main :: IO ()
 main = interact (selector . lines)
   where selector ("part1":ws) = part1 ws
         selector ("part2":ws) = part2 ws
-        part2 _ = show 123
 
 part1 ws = show $ sum $ zipWith sumProductSelector operators $ foldTuples $ map toTuples $ map readNumberLine $ filter (not . isOperatorLine) ws
   where foldTuples ts = foldl (zipWith sumProduct) (replicate (length operators) (0,1)) ts
         operators = words $ last ws
-        isOperatorLine line = '*' `elem` line || '+' `elem` line
         sumProductSelector "+" = fst
         sumProductSelector "*" = snd
+
+part2 ws = show $ sum $ zipWith sumProductSelector operators $ map (map read) $ splitOn [delim] $ transpose $ filter (not . isOperatorLine) ws
+  where operators = words $ last ws
+        delim = replicate (length ws - 1) ' '
+        sumProductSelector "+" = sum
+        sumProductSelector "*" = product
+
+isOperatorLine :: String -> Bool
+isOperatorLine line = '*' `elem` line || '+' `elem` line
 
 readNumberLine :: String -> [Integer]
 readNumberLine = map read . words
